a6edd5afe10d6474ab3966a9c692afa97f50f6be
gwanwoo-you <skymill2000@gmail.com>
2020-07-08 16:27:53 +0900

    이벤트 바인딩

--- expressServer.js
+++ expressServer.js
@@ -4,6 +4,9 @@ const app = express();
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
@@ -13,6 +16,12 @@ app.get("/ejsTest", function (req, res) {
 });
 
 app.post("/ajaxTest", function (req, res) {
+  var userId = req.body.sendUserId;
+  var userPassword = req.body.sendUserPassword;
+  console.log("요청 바디 :", req.body);
+  console.log("사용자 아이디는 :", userId);
+  console.log("사용자 password :", userPassword);
+
   res.json("로그인에 성공하셨습니다.");
 });
 
