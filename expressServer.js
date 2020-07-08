1d28a01d370fbcfc85c929c2547487eea810d191
gwanwoo-you <skymill2000@gmail.com>
2020-07-08 15:17:30 +0900

    ejs 템플릿 사용 선언완료

---
+++ expressServer.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/addRouter", function (req, res) {
+  console.log("router working");
+  res.send("<html><h1>안녕하세요 html 코드입니다. </h1></html>");
+});
+
+app.listen(3000);
