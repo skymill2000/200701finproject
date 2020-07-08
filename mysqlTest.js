96af2cd171cfe185b75da557b150e736b01d9223
gwanwoo-you <skymill2000@gmail.com>
2020-07-08 14:36:13 +0900

    데이터베이스 연동 실습 완료

---
+++ mysqlTest.js
@@ -0,0 +1,22 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1q2w3e4r",
+  database: "fintech",
+  port: "3306",
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM fintech.user", function (
+  error,
+  results,
+  fields
+) {
+  if (error) throw error;
+  console.log("우리 회원들은 : ", results);
+});
+
+connection.end();
