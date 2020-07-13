fb96eb0037a3e8907002292928dfd4196038020a
gwanwoo-you <skymill2000@gmail.com>
2020-07-13 13:14:36 +0900

    회원 가입 페이지 제작

--- expressServer.js
+++ expressServer.js
@@ -10,31 +10,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public"))); //to use static asset
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
-
-app.get("/ejsTest", function (req, res) {
-  res.render("test");
-});
-
-app.get("/designTest", function (req, res) {
-  res.render("designSample.ejs");
-});
-
-app.post("/ajaxTest", function (req, res) {
-  var userId = req.body.sendUserId;
-  var userPassword = req.body.sendUserPassword;
-  console.log("요청 바디 :", req.body);
-  console.log("사용자 아이디는 :", userId);
-  console.log("사용자 password :", userPassword);
-
-  res.json("로그인에 성공하셨습니다.");
-});
-
-app.get("/addRouter", function (req, res) {
-  console.log("router working");
-  res.send("<html><h1>안녕하세요 html 코드입니다. </h1></html>");
+app.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
 app.listen(3000);
--- requestExample.js
+++ requestExample.js
@@ -4,7 +4,7 @@ var parseString = require("xml2js").parseString;
 var url =
   "http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109";
 request(url, function (error, response, body) {
-  //   console.log("body:", body); // Print the HTML for the Google homepage.
+  //console.log("body:", body); // Print the HTML for the Google homepage.
   parseString(body, function (err, result) {
     console.dir(result.rss.channel[0].item[0].description[0].header[0].wf[0]);
   });
