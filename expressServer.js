210da1d06b45f65c12d9a992a943496466022c65
gwanwoo-you <skymill2000@gmail.com>
2020-07-13 14:50:51 +0900

    데이터베이스 입력 완성

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const request = require("request");
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1q2w3e4r",
+  database: "fintech",
+  port: "3306",
+});
+
+connection.connect();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -48,6 +58,32 @@ app.get("/authResult", function (req, res) {
 
 app.post("/signup", function (req, res) {
   console.log(req.body);
+  var userName = req.body.userName;
+  var userPassword = req.body.userPassword;
+  var userEmail = req.body.userEmail;
+  var userAccessToken = req.body.userAccessToken;
+  var userRefreshToken = req.body.userRefreshToken;
+  var userSeqNo = req.body.userSeqNo;
+
+  var sql =
+    "INSERT INTO user (`name`, `email`, `password`, `accesstoken`, `refreshtoken`, `userseqno`) VALUES (?, ?, ?, ?, ?, ?)";
+  connection.query(
+    sql,
+    [
+      userName,
+      userEmail,
+      userPassword,
+      userAccessToken,
+      userRefreshToken,
+      userSeqNo,
+    ],
+    function (error, results) {
+      if (error) throw error;
+      else {
+        res.json(1);
+      }
+    }
+  );
 });
 
 app.listen(3000);
