534bfdde09d79352f27b1555be25ed013d19aecc
gwanwoo-you <skymill2000@gmail.com>
2020-07-22 13:30:09 +0900

    스크래퍼 멜론 차트 정보

--- scraping/Melon.py
+++ scraping/Melon.py
@@ -3,17 +3,12 @@ from selenium import webdriver
 driver = webdriver.Chrome('./chromedriver')
 driver.implicitly_wait(3)
 driver.get(
-    'https://www.alimi.or.kr/api/a/vacant/selectApiVacant.do')
-table = driver.find_element_by_class_name('search_list')
-tableBody = table.find_element_by_tag_name('tbody')
-rows = tableBody.find_elements_by_tag_name('tr')
-for index, value in enumerate(rows):
+    'https://www.melon.com/chart/index.htm')
+tableBody = driver.find_element_by_xpath('//*[@id="frm"]/div/table/tbody')
+tableRow = tableBody.find_elements_by_tag_name('tr')
+for index, value in enumerate(tableRow):
     if index != 0:
         rowNo = value.find_elements_by_tag_name('td')[1]
         address = value.find_elements_by_tag_name('td')[2]
         homeType = value.find_elements_by_tag_name('td')[3]
         print(rowNo.text + address.text + homeType.text)
-
-nextBtn = driver.find_element_by_xpath(
-    '//*[@id="list"]/tbody/tr[4]/td/ul/a[1]')
-nextBtn.click()
